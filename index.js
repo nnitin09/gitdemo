c165d4ddbbeaa57a0c6277018412435e0d0b9db6
Nitin <nitnkumarmaurya07@gmail.com>
2026-04-12 13:48:37 +0530

    Added message-style boxes and improved UI

--- index.js
+++ index.js
@@ -2,23 +2,25 @@ const input = document.querySelector("#text");
 const btn = document.querySelector("#btn");
 const task = document.querySelector("#task");
 const one = document.querySelector("#one");
+const container = document.querySelector("#container");
+
 
 btn.addEventListener("click", ()=>{
   value = input.value;
 
   if(!value) return;
 
-  let li = document.createElement("li");
-  li.innerText = value;
-  task.appendChild(li);
+  let div = document.createElement("div");
+  div.innerText = value;
+  task.appendChild(div);
   
   let button = document.createElement("button");
-  button.innerText = "❌";
+  button.innerText = " ❌";
   button.addEventListener("click", ()=>{
-    li.remove();
+    div.remove();
   });
 
-  li.appendChild(button);
+  div.appendChild(button);
   
   one.style.display="flex";
   one.style.flexDirection="column";
@@ -28,5 +30,25 @@ btn.addEventListener("click", ()=>{
   one.style.backgroundColor="pink";
   one.style.width="500px";
   
+  container.style.display = "flex";
+  container.style.justifyContent = "center";
+  container.style.alignItems = "center";
+  container.style.height = "100vh";
+  
+  div.style.color="white";
+  div.style.borderRadius = "5px";
+  div.style.alignItems = "center";
+  div.style.display = "flex";
+  div.style.width = "fit-content";
+  div.style.maxWidth = "70%";
+  div.style.justifyContent = "space-between";
+  div.style.backgroundColor = "green";
+  div.style.padding = "10px";
+  div.style.marginTop = "5px";
+  div.style.marginLeft = "auto";
+  
+  input.value="";
   
 })
+
+
